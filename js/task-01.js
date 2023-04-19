7ef283362a2e828e9e7135dfc5434095b83d77bf
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-04-19 18:35:51 +0200

    111

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,26 @@
+// const categoriesList = document.querySelector('#categories');
+// const categoriesItems = categoriesList.querySelectorAll('.item');
+
+// console.log(`Number of categories: ${categoriesItems.length}`);
+
+// categoriesItems.forEach(category => {
+//   const categoryName = category.querySelector('h2').textContent;
+//   const categoryElements = category.querySelectorAll('li');
+//   console.log(`Category: ${categoryName}`);
+//   console.log(`Elements: ${categoryElements.length}`);
+// });
+
+
+
 const categoriesList = document.querySelector('#categories');
-const categoriesItems = categoriesList.querySelectorAll('.item');
+const categoriesItems = categoriesList.children;
 
 console.log(`Number of categories: ${categoriesItems.length}`);
 
-categoriesItems.forEach(category => {
+Array.from(categoriesItems).forEach(category => {
   const categoryName = category.querySelector('h2').textContent;
   const categoryElements = category.querySelectorAll('li');
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryElements.length}`);
 });
+
